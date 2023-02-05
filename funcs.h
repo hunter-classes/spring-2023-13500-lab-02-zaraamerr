6c877dd0a8cc8444b2567774cdc2e4f02714622f
zaraamerr <zaraa.amerr@gmail.com>
2023-02-04 23:06:36 -0500

    added funcs.h

---
+++ funcs.h
@@ -0,0 +1,11 @@
+//Name: Zara Amer
+//Course Name: CSCI 135
+//Date: 2/4/23
+//Assignment: Task B for Lab 2
+
+#pragma once
+
+//IMPORTANT .h (include) files should ONLY contain
+//prototypes and definitions, not runnable code
+
+int print_interval (int L, int U); 
